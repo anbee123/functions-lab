b1b22a173d5dbd7bb2bc01e9eea642299719b587
An Bui <anhipchipchip@gmail.com>
2023-01-28 17:51:28 -0500

    finish exercise 2

---
+++ functions.py
@@ -0,0 +1,9 @@
+
+
+
+#2.Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+def largest(nums):
+  return max(nums)
+ 
+print(largest([10, 4, 2, 231, 400, 54]) )
+print(largest([1, 2, 3, 4, 0]))
