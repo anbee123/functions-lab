f61ce3b27b3d094150c663d637464ca2498e7c63
An Bui <anhipchipchip@gmail.com>
2023-02-03 21:58:35 -0500

    finished challenge 3, and added one more solution for q2

--- functions.py
+++ functions.py
@@ -1,9 +1,34 @@
-
-
+# Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
+def sum_to (num):
+  sum = 0
+  for i in range(num+1):
+    sum+=i
+  return sum
+print(sum_to(6)) 
+print(sum_to(10)) 
 
 #2.Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+### way 1###
 def largest(nums):
   return max(nums)
+
+### way2 ###
+def largest(list):
+    current = 0
+    largest_num = 0 
+    for num in list:
+        if current < num:
+            current = num
+            largest_num = current
+    return largest_num 
  
 print(largest([10, 4, 2, 231, 400, 54]) )
 print(largest([1, 2, 3, 4, 0]))
+
+#3.Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+def occurrences(str1, str2):
+    return str1.count(str2)
+    
+print(occurrences('fleep floop', 'p'))
+
+# 4.Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product.
